9ab1c0c92066e3046fe8cb87bab51ae2863e7a74
Sunny Golovine <sunny@glvn.co>
2022-09-05 19:27:47 -0600

    feat: add subpage nav for resume page

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -4,6 +4,7 @@ import { featureStubs } from "~/config/featureStubs"
 import { ResumePage } from "~/features/resume"
 import { useFeatureRedirect } from "~/hooks/useFeatureRedirect"
 import { AppPage } from "~/model/PageProps"
+import labels from "~/labels.json"
 
 const Page: AppPage = () => {
   useFeatureRedirect({
@@ -14,7 +15,17 @@ const Page: AppPage = () => {
   return <ResumePage />
 }
 Page.getLayout = page => (
-  <PageLayout header={{ title: "Resume" }}>{page}</PageLayout>
+  <PageLayout
+    header={{
+      title: labels.headerRoutes.resume,
+      pageNavigation: [
+        { id: "download-pdf", title: labels.resume.downloadPDF, href: "#" },
+        { id: "download-doc", title: labels.resume.downloadDOC, href: "#" },
+      ],
+    }}
+  >
+    {page}
+  </PageLayout>
 )
 
 export default Page
