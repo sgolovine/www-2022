e43492d330e6f464cbfbd4cc1ba7543634ba23d1
Sunny Golovine <sunny@glvn.co>
2022-09-14 13:34:37 -0600

    feat(punk): adjust layout, add images, put behind flag

--- src/components/punk/Punk.tsx
+++ src/components/punk/Punk.tsx
@@ -4,8 +4,8 @@ import { usePunk } from "./usePunk"
 import Image from "next-image-export-optimizer"
 import PunkCSS from "./Punk.module.css"
 
-const eyesImage = "/images/eyes.png"
-const sunglassesImage = "/images/sunglasses.png"
+const eyesImage = "/images/punk/eyes.png"
+const sunglassesImage = "/images/punk/sunglasses.png"
 
 const Punk = () => {
   const {
@@ -18,40 +18,41 @@ const Punk = () => {
   } = usePunk()
 
   return (
-    <div className={PunkCSS.PunkBorderContainer}>
-      <div
-        onClick={onBackgroundClick}
-        className={PunkCSS.PunkContainer}
-        style={{ backgroundColor: bgColor }}
-      >
-        <div className={PunkCSS.PunkContainerTop}>
-          <button
-            type="button"
-            className={PunkCSS.PunkButton}
-            onClick={toggleShades}
-          >
-            <div>
-              {showShades ? (
-                <Image src={eyesImage} alt="eyes" height={48} width={48} />
-              ) : (
-                <Image
-                  src={sunglassesImage}
-                  alt="shades"
-                  height={48}
-                  width={48}
-                />
-              )}
-              {/* TODO: Fix Images  */}
+    <div className={PunkCSS.OuterContainer}>
+      <div className={PunkCSS.PunkBorderContainer}>
+        <div
+          onClick={onBackgroundClick}
+          className={PunkCSS.PunkContainer}
+          style={{ backgroundColor: bgColor }}
+        >
+          <div className={PunkCSS.PunkContainerTop}>
+            <button
+              type="button"
+              className={PunkCSS.PunkButton}
+              onClick={toggleShades}
+            >
+              <div>
+                {showShades ? (
+                  <Image src={eyesImage} alt="eyes" height={48} width={48} />
+                ) : (
+                  <Image
+                    src={sunglassesImage}
+                    alt="shades"
+                    height={48}
+                    width={48}
+                  />
+                )}
+              </div>
+            </button>
+          </div>
+          <div className={PunkCSS.PunkContainerBottom}>
+            <div className={PunkCSS.PunkSvgContainer}>
+              <PunkSVG
+                showShades={showShades}
+                eyeColorLeft={eyeColorLeft}
+                eyeColorRight={eyeColorRight}
+              />
             </div>
-          </button>
-        </div>
-        <div className={PunkCSS.PunkContainerBottom}>
-          <div className={PunkCSS.PunkSvgContainer}>
-            <PunkSVG
-              showShades={showShades}
-              eyeColorLeft={eyeColorLeft}
-              eyeColorRight={eyeColorRight}
-            />
           </div>
         </div>
       </div>
---
+++ src/components/punk/index.ts
@@ -0,0 +1,1 @@
+export { default as Punk } from "./Punk"
