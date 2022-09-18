b9eda4b9e696b627300668d61647896d74743317
Sunny Golovine <sunny@glvn.co>
2022-09-18 12:50:05 -0600

    feat(ci): remove tsc from lint staged, add tsc as step in pre-commit after lint staged

--- src/components/Image.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Image = void 0;
-var next_image_export_optimizer_1 = require("next-image-export-optimizer");
-exports.Image = next_image_export_optimizer_1["default"];
-console.log("test");
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,3 +1,5 @@
 import ExportedImage from "next-image-export-optimizer"
 
+console.log("testing")
+
 export { ExportedImage as Image }
