6689cf3d52a8a0a77eafbe0c34d11d0a13878229
Sunny Golovine <sunny@glvn.co>
2022-09-18 12:48:35 -0600

    feat(ci): remove test files

---
+++ src/components/Image.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.Image = void 0;
+var next_image_export_optimizer_1 = require("next-image-export-optimizer");
+exports.Image = next_image_export_optimizer_1["default"];
+console.log("test");
