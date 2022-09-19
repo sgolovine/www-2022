757f4bb6517ae042feceaa285721765e1690da23
Sunny Golovine <sunny@glvn.co>
2022-09-19 16:48:53 -0600

    feat(functions): add build command to netlify.toml

--- functions/send-email.ts
+++ functions/send-email.ts
@@ -1,4 +1,4 @@
-const handler = async (_event, _context, callback) => {
+const handler = async (_event: any, _context: any, callback: any) => {
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
