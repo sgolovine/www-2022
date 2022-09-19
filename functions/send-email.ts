53abaabae4f4ab4041c2bbfd9f5f425bc62296b0
Sunny Golovine <sunny@glvn.co>
2022-09-19 17:26:39 -0600

    feat(functions): use native ts for functions

--- functions/send-email.ts
+++ functions/send-email.ts
@@ -1,10 +1,12 @@
-const handler = async (_event: any, _context: any, callback: any) => {
-  callback(null, {
+import { Handler } from "@netlify/functions"
+
+const handler: Handler = async () => {
+  return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Hello World",
     }),
-  })
+  }
 }
 
 export default handler
