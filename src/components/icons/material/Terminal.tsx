90550989c92f19b058f52ee8a8e7d872dd975a62
Sunny Golovine <sunny@glvn.co>
2022-10-02 08:27:48 -0600

    chore(tailwindcss): remove daisyui

--- src/components/icons/material/Terminal.tsx
+++ src/components/icons/material/Terminal.tsx
@@ -3,7 +3,6 @@ import { IconProps } from "~/model/IconProps"
 export const Terminal: React.FC<IconProps> = ({ className }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    enable-background="new 0 0 24 24"
     viewBox="0 0 24 24"
     className={className}
   >
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,17 +13,5 @@ module.exports = {
   variants: {
     extends: {},
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
-  // For not everything is disabled except for the essentials
-  // We need for the custom components daisyui provides.
-  daisyui: {
-    styled: true,
-    themes: false,
-    base: false,
-    utils: false,
-    logs: false,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
+  plugins: [require("@tailwindcss/typography")],
 }
