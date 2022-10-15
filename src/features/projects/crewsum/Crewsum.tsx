053b0d9090e42cf2a66c40bdf9e5530930dab2b8
Sunny Golovine <sunny@glvn.co>
2022-10-15 09:25:39 -0600

    feat(crewsum-page): wip

---
+++ src/features/projects/crewsum/Crewsum.tsx
@@ -0,0 +1,9 @@
+const Crewsum = () => {
+  return (
+    <div>
+      <p>Crewsum Project Page</p>
+    </div>
+  )
+}
+
+export default Crewsum
---
+++ src/features/projects/crewsum/index.ts
@@ -0,0 +1,1 @@
+export { default as CrewsumPage } from "./Crewsum"
