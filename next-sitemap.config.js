12886ac93003d083858755ca596b856fb64d2bd5
Sunny Golovine <sunny@glvn.co>
2022-10-17 15:03:45 -0600

    feat(sitemap): add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://sunny.gg",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "daily",
+  exclude: [],
+}
